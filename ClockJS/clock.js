bdfc542dd1f86133c5086a5a3d86e7505ec0c84d
chanho Yoon <chanho.yoon.1993@gmail.com>
2019-12-17 18:46:19 +0900

    clockTitle.innerText  AM/PM
    AM/PM으로 시간 출력을 위한 수정

--- ClockJS/clock.js
+++ ClockJS/clock.js
@@ -1,12 +1,21 @@
 const clockContainer = document.querySelector(".js-clock");
 clockTitle = clockContainer.querySelector("h1")
+
 function getTime() {
     const date = new Date()
     const minutes = date.getMinutes()
     const hours = date.getHours()
     const seconds = date.getSeconds()
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+
+    if (hours < 12) {
+        clockTitle.innerText = `AM / ${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+    } else if (hours == 12) {
+        clockTitle.innerText = `PM / ${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+    } else {
+        clockTitle.innerText = `PM / ${hours < 10 ? `0${hours%13+1}` : `${hours%13+1}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+    }
 }
+
 function init() {
     getTime()
     setInterval(getTime, 1000)
