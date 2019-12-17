4746d485477126478876f60b15d198155ade044f
chanho Yoon <chanho.yoon.1993@gmail.com>
2019-12-18 00:39:46 +0900

    시계 AM / PM 12시간제로 변경
    시간이 12시보다 작을 경우 앞에 0을 붙여주고 12시일 경우 PM인 동시에 12시 그대로 출력하도록 하고 이후에는 22시 되기 전까지는 일의자리 이기 때문에 조건문으로 시간앞에 0을 붙여줌

--- ClockJS/clock.js
+++ ClockJS/clock.js
@@ -6,13 +6,12 @@ function getTime() {
     const minutes = date.getMinutes()
     const hours = date.getHours()
     const seconds = date.getSeconds()
-
     if (hours < 12) {
         clockTitle.innerText = `AM / ${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
     } else if (hours == 12) {
         clockTitle.innerText = `PM / ${hours}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
     } else {
-        clockTitle.innerText = `PM / ${hours > 10 ? `0${hours%13+1}` : `${hours%13+1}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+        clockTitle.innerText = `PM / ${hours < 22  ? `0${hours%13+1}` : `${hours%13+1}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
     }
 }
 
@@ -20,4 +19,5 @@ function init() {
     getTime()
     setInterval(getTime, 1000)
 }
+
 init()
