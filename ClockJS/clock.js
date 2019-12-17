fc5a196962feb2fbb4ddb73650e9cc1b03dad480
chanho Yoon <chanho.yoon.1993@gmail.com>
2019-12-17 18:55:14 +0900

    clockTitle.innerText AM / PM
    시간 AM / PM으로 나눠서 표시하기 위한 코드 수정

--- ClockJS/clock.js
+++ ClockJS/clock.js
@@ -10,9 +10,9 @@ function getTime() {
     if (hours < 12) {
         clockTitle.innerText = `AM / ${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
     } else if (hours == 12) {
-        clockTitle.innerText = `PM / ${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+        clockTitle.innerText = `PM / ${hours}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
     } else {
-        clockTitle.innerText = `PM / ${hours < 10 ? `0${hours%13+1}` : `${hours%13+1}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+        clockTitle.innerText = `PM / ${hours > 10 ? `0${hours%13+1}` : `${hours%13+1}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
     }
 }
 
