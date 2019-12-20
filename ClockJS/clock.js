48cb0595025a5eb6326d187e3f255f6ecef69d79
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-12-20 19:39:52 +0900

    Update clock.js

--- ClockJS/clock.js
+++ ClockJS/clock.js
@@ -1,23 +1,27 @@
-const clockContainer = document.querySelector(".js-clock");
-clockTitle = clockContainer.querySelector("h1")
+const clockContainer = document.querySelector('.js-clock')
+const clockTitle = clockContainer.querySelector('h1')
 
 function getTime() {
-    const date = new Date()
-    const minutes = date.getMinutes()
-    const hours = date.getHours()
-    const seconds = date.getSeconds()
-    if (hours < 12) {
-        clockTitle.innerText = `AM / ${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
-    } else if (hours == 12) {
-        clockTitle.innerText = `PM / ${hours}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
-    } else {
-        clockTitle.innerText = `PM / ${hours < 22  ? `0${hours%13+1}` : `${hours%13+1}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
-    }
+  const date = new Date()
+  const minutes = date.getMinutes()
+  const hours = date.getHours()
+  const seconds = date.getSeconds()
+  if (hours < 12) {
+    clockTitle.innerText = `AM / ${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${
+      seconds < 10 ? `0${seconds}` : `${seconds}`
+    }`
+  } else if (hours == 12) {
+    clockTitle.innerText = `PM / ${hours}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}` : `${seconds}`}`
+  } else {
+    clockTitle.innerText = `PM / ${hours < 22 ? `0${(hours % 13) + 1}` : `${(hours % 13) + 1}`}:${minutes < 10 ? `0${minutes}` : `${minutes}`}:${
+      seconds < 10 ? `0${seconds}` : `${seconds}`
+    }`
+  }
 }
 
 function init() {
-    getTime()
-    setInterval(getTime, 1000)
+  getTime()
+  setInterval(getTime, 1000)
 }
 
 init()
