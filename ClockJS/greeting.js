b2d4ad4031e4cdb7c8e8357f8647af8b312b0ee5
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-12-20 19:39:45 +0900

    Create greeting.js
    local storage에 값이 있냐 없냐에 따른 보여주고 안보여주고 작성

---
+++ ClockJS/greeting.js
@@ -0,0 +1,35 @@
+const form = document.querySelector('.js-form'),
+  input = form.querySelector('input'),
+  greeting = document.querySelector('.js-greetings')
+const USER_LS = 'currentUser'
+//get class name
+const SHOWING_CN = 'showing'
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN)
+  greeting.classList.add(SHOWING_CN)
+  greeting.innerText = `hello ${text}`
+}
+
+function paintNotGreeting() {
+  form.classList.add(SHOWING_CN)
+  input.classList.add(SHOWING_CN)
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS)
+
+  if (currentUser === null) {
+    //login no
+    paintNotGreeting()
+  } else {
+    //login ok
+    paintGreeting(currentUser)
+  }
+}
+
+function init() {
+  loadName()
+}
+
+init()
