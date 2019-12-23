100c6aafe1a9b0d022e295b7bfed70e7c1de0544
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-12-24 01:16:21 +0900

    button 삭제처리
    button 클릭시 해당 li 제거 , localStorage 해당 value 값 제거

--- ClockJS/todo.js
+++ ClockJS/todo.js
@@ -1,8 +1,22 @@
 const toDoForm = document.querySelector('.js-toDoForm'),
   toDoInput = toDoForm.querySelector('input'),
   toDoList = document.querySelector('.js-toDoList')
+
 const TODOS_LS = 'toDos'
 let toDos = []
+
+function deleteToDo(event) {
+  const btn = event.target
+  const li = btn.parentNode
+  toDoList.removeChild(li)
+  console.log(`filter 처리 하기 전 toDos 값 ${JSON.stringify(toDos)}`)
+  const cleanToDos = toDos.filter(function(toDo) {
+    return toDo.id !== parseInt(li.id)
+  })
+  console.log(`cleanToDos 값 ${JSON.stringify(cleanToDos)}`)
+  toDos = cleanToDos
+  saveToDos()
+}
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
 }
@@ -11,8 +25,8 @@ function paintToDo(text) {
   const delBtn = document.createElement('button')
   const span = document.createElement('span')
   const newId = toDos.length + 1
-
   delBtn.innerText = '❌'
+  delBtn.addEventListener('click', deleteToDo)
   span.innerText = text
   li.appendChild(delBtn)
   li.appendChild(span)
