af491ae11cca77ebcf56fadb63a2b833483d402f
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-12-25 00:34:09 +0900

    날씨 API를 사용
    fetch 사용법과 .then 숙지

--- ClockJS/js/weather.js
+++ ClockJS/js/weather.js
@@ -1,5 +1,21 @@
+const weather = document.querySelector('.js-weather')
+
 const COORDS = 'coords'
 const API_KEY = getWeatherApiKey()
+function getWeather(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+    .then(function(response) {
+      //Response안에 있는 내용 가져옴
+      return response.json()
+    })
+    .then(function(json) {
+      //json 준비가 되면
+      const temperature = json.main.temp
+      const place = json.name
+      weather.innerText = `${temperature}℃ - ${place}`
+    })
+}
+
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj))
 }
@@ -12,20 +28,24 @@ function handleGeoSucces(position) {
     longitude
   }
   saveCoords(coordsObj)
+  getWeather(latitude, longitude)
 }
 function handleGeoError(position) {
   console.log('위치를 가져오지 못했습니다.')
 }
 function askForCoords() {
+  //지역 위치정보에 대해 가져옴 성공시 succes 실패시 error 실행
   navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
 }
 
 function loadCoords() {
-  const loadedCords = localStorage.getItem(COORDS)
-  if (loadedCords === null) {
+  const loadedCoords = localStorage.getItem(COORDS)
+  if (loadedCoords === null) {
     askForCoords()
   } else {
     //getWeather
+    const parseCoords = JSON.parse(loadedCoords)
+    getWeather(parseCoords.latitude, parseCoords.longitude)
   }
 }
 
