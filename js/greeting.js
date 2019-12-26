e7da502733a3c15c748227d2270ebaeebba449ea
chanho-Yoon <chanho.yoon.1993@gmail.com>
2019-12-26 20:08:33 +0900

    로그인 했을 때와 안했을 때 display:none 기능
    로그인한 아이디에 logoutBtn 이벤트 발생시 해당하는 span, button 태그를 지우고 , removeuser() 실행하여 localStorage에서 currentUser 삭제 및 일부 클래스 숨김처리

--- js/greeting.js
+++ js/greeting.js
@@ -1,21 +1,51 @@
 const form = document.querySelector('.js-form'),
   input = form.querySelector('input'),
-  greeting = document.querySelector('.js-greetings'),
-  logout = document.querySelector('.js-logout')
+  greeting = document.querySelector('.js-greetings')
+//로그인 했을 시에 form과 title , list가 보여지도록 하기 위한 변수
+const toDoFormShow = document.querySelector('.js-toDoForm'),
+  toDoListShow = document.querySelector('.js-toDoList'),
+  toDoTitleShow = document.querySelector('.toDoTitle')
 const USER_LS = 'currentUser'
 //get class name
 const SHOWING_CN = 'showing'
+const SHOWING_TODO = 'showing-toDo'
 
 function paintGreeting(text) {
+  const span = document.createElement('span')
+  const logoutBtn = document.createElement('button')
   form.classList.remove(SHOWING_CN)
   greeting.classList.add(SHOWING_CN)
-  greeting.innerText = `안녕하세요 ${text} 님`
+  span.innerText = `안녕하세요 ${text} 님`
+  logoutBtn.innerText = '❌'
+  logoutBtn.addEventListener('click', userLogout)
+  greeting.append(span)
+  greeting.appendChild(logoutBtn)
+  //로그인 했을 시 보이도록
+  toDoFormShow.classList.add(SHOWING_CN)
+  toDoTitleShow.classList.add(SHOWING_CN)
+  toDoListShow.classList.add(SHOWING_TODO)
 }
 
 function saveName(text) {
   localStorage.setItem(USER_LS, text)
 }
-
+//지웠을 때 다시 일부 메뉴 안보이도록
+function removeuser() {
+  localStorage.removeItem(USER_LS)
+  toDoFormShow.classList.remove(SHOWING_CN)
+  toDoTitleShow.classList.remove(SHOWING_CN)
+  toDoListShow.classList.remove(SHOWING_TODO)
+  form.classList.add(SHOWING_CN)
+  input.value = ''
+}
+//currentUser 삭제 함수
+function userLogout(event) {
+  const span = document.querySelector('span')
+  const button = document.querySelector('button')
+  greeting.removeChild(span)
+  greeting.removeChild(button)
+  removeuser()
+}
 function handleSubmit(event) {
   event.preventDefault()
   const currentValue = input.value
